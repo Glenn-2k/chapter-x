85edfde33fdfe53a1a0428c8af2137c76c5054cc
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-10-18 14:49:47 +0200

    Refactor Header component to use flexbox for responsive navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="bg-black p-4">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex flex-row justify-between items-center">
         <div className="flex items-center space-x-4">
           <img
             src="src/assets/ChapterXlogo.png"
@@ -12,25 +12,25 @@ const Header = () => {
           />
           <h1 className="text-white">Chapter X</h1>
         </div>
-        <nav>
-          <ul className="flex space-x-4">
+        <nav className="flex-grow">
+          <ul className="flex flex-row justify-end space-x-4">
             <li>
-              <a href="#" className="text-white">
+              <a href="#" className="text-white hover:text-gray-300">
                 Home
               </a>
             </li>
             <li>
-              <a href="#" className="text-white">
+              <a href="#" className="text-white hover:text-gray-300">
                 About
               </a>
             </li>
             <li>
-              <a href="#" className="text-white">
+              <a href="#" className="text-white hover:text-gray-300">
                 Services
               </a>
             </li>
             <li>
-              <a href="#" className="text-white">
+              <a href="#" className="text-white hover:text-gray-300">
                 Contact
               </a>
             </li>
